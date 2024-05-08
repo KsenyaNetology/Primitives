d57d5d97e0c42bd02ecbe53efbb7835b90a5e4d5
Neto test <test@neto.com>
2024-05-08 12:52:56 +0300

    add src/

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 8999; // стоимость билета
+        int condition = 20; // количество рублей для одной бонусной милли
+        int milli = price / condition; // количество начисленных миль за купленный билет
+
+        System.out.println("Начислится милль: " + milli);
+    }
+}
+
